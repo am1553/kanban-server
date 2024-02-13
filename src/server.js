306d5d45269a2cfad0f53ad38b4caa888fdf54d8
Aryan Mehta <aryanmehta2016@gmail.com>
2024-02-13 23:12:18 +0000

    .

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { protect } from "./modules/auth.js";
 import router from "./router/index.js";
 const app = express();
 const corsOptions = {
-  origin: "https://kanban-react-app.onrender.com/",
+  origin: "https://kanban-react-app.onrender.com/*",
   methods: ["GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
